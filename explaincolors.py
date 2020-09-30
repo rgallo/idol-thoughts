8f84a3ab9919b82bf811ed54db34826068392254
Ryan Gallo <ryan.j.gallo@gmail.com>
2020-09-30 15:50:49 -0400

    Rewritten color explanation

--- explaincolors.py
+++ explaincolors.py
@@ -15,13 +15,14 @@ PURPLE = 9442302
 BLACK = 1
 
 output = [
-    ('**Red Hot:** Greater than the max non-shutout value (Only shutouts ever recorded at this value)', PINK),
-    ("**Hot:** Less than or equal to the max non-shutout value and greater than or equal to (average + 3σ [three standard deviations]) shutout value (most likely shutout that isn't a guarantee)", RED),
-    ("**Warm:** Less than (average + 3σ) shutout value and greater than or equal to (average + 3σ) non-shutout value", ORANGE),
-    ("**Tepid:** Less than (average + 3σ) non-shutout value and greater than or equal to average shutout value", YELLOW),
-    ("**Temperate:** Less than average shutout value and greater than or equal to average non-shutout value", GREEN),
-    ("**Cool:** Less than average non-shutout value and greater than or equal to min shutout value (still possible, but unlikely)", BLUE),
-    ("**Dead Cold:** Less than min shutout value (No shutouts ever recorded at this value)", PURPLE)
+    ('**Red Hot:** Higher than the lowest calculated shutout value at which zero non-shutouts have ever been recorded. (Historical: 100%)', PINK),
+    ("**Hot:** At or below the maximum value at which a non-shutout has been recorded, but shutouts recorded at these values are at least significantly\* above the mean recorded value for shutouts. (Historical 15.4%)", RED),
+    ("**Warm:** Lower than significantly above the mean recorded value for shutouts, but at or above a value of significantly above the mean recorded value for non-shutouts. (Historical: 9.78%)", ORANGE),
+    ("**Tepid:** At or above the mean recorded value for shutouts, but also between the mean and significantly above of the mean recorded value for non-shutouts. (Historical: 6.89%)", YELLOW),
+    ("**Temperate:** Below the mean recorded value for shutouts, but above the mean recorded value for non-shutouts. (Historical: 5.25%)", GREEN),
+    ("**Cool:** At or above the minimum recorded value for shutouts, but below the mean recorded value for non-shutouts. (Historical: 2.93%)", BLUE),
+    ("**Dead Cold:** Below the minimum recorded value for shutouts. (Historical: 0%)", PURPLE),
+    ("\* Significant: Three standard deviations\nHistorical data spans middle of season 6 to near-end of season 8\nAll pitcher evaluations ever evaluated by us = 4.40% pitched shutouts", None)
 ]
 webhook = DiscordWebhook(url=os.getenv("DISCORD_WEBHOOK_URL").split(";"))
 for line, color in output:
