6da007f050b6af57daf6715bf57d487480e4c1b6
madjayem <71520186+madjayem@users.noreply.github.com>
2020-10-18 15:44:59 -0500

    Update explaincolors.py

--- explaincolors.py
+++ explaincolors.py
@@ -20,8 +20,8 @@ output = [
     ("**Warm:** Lower than Hot, but still some amount above the mean recorded value for nonshutouts. (Historical: 20%, 80 pitchers)", ORANGE),
     ("**Tepid:** Lower than Warm, but still above the mean value for shutouts. (Historical: 10.02%, 649 pitchers)", YELLOW),
     ("**Temperate:** Lower than Tepid, but above the mean recorded value for non-shutouts. (Historical: 5.79%, 1,520 pitchers)", GREEN),
-    ("**Cool:** At or above the minimum recorded value for shutouts, but below the mean recorded value for non-shutouts. (Historical: 3.81%, 3,856 pitchers)", BLUE),
-    ("**Dead Cold:** Below the minimum recorded value for shutouts. (Historical: 0%, 196 pitchers)", PURPLE),
+    ("**Cool:** At or above the minimum recorded value for shutouts, but below the mean recorded value for non-shutouts. (Historical: 4.12%, 3,568 pitchers)", BLUE),
+    ("**Dead Cold:** Below the minimum recorded value for shutouts. (Historical: 0%, 484 pitchers)", PURPLE),
     ("All pitcher evaluations ever evaluated by us = 4.71% pitched shutouts", None)
 ]
 webhook = DiscordWebhook(url=os.getenv("DISCORD_WEBHOOK_URL").split(";"))
